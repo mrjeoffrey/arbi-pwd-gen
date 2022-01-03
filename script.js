f5d8b0cfd7cc52c0f1634a3cb449803f1dde05bd
mrjeoffrey <b.jeoffrey@gmail.com>
2022-01-03 14:37:59 -0800

    Added password parameters via confirm prompt

--- script.js
+++ script.js
@@ -18,21 +18,27 @@ var inputSpecialChar;
 // Define generatePassword
 function generatePassword() {
 // 1. Prompt user for password criteria
-//    a. Length 8 - 128 char
-  var inputLength = (prompt("Enter the character length for your password."));
-  // Error if not between 8 - 128 character range.
-  while (inputLength <= 7 || inputLength >= 129) {
-    alert("Try again. Password length must be between 8 - 128 characters.");
+//  a. Length 8 - 128 char
     var inputLength = (prompt("Enter the character length for your password."));
-  }
+    // Validate Input - Error if not between 8 - 128 character range.
+    while (inputLength <= 7 || inputLength >= 129) {
+      alert("Try again. Password length must be between 8 - 128 characters.");
+      var inputLength = (prompt("Enter the character length for your password."));
+    }
 
-  // If user input in range, show length of password characters.
-  alert("Password length will be " + inputLength + " characters long.");
+    // If user input in range, show length of password characters.
+    alert("Password length will be " + inputLength + " characters long.");
+  
+//  b. lowercase
+    var inputLowerCase = confirm("Add lowercase characters? hit 'OK' to include lowercase characters. hit 'Cancel' to not include.");
+//  c. uppercase
+    var inputUpperCase = confirm("Add UPPERCASE characters? hit 'OK' to include lowercase characters. hit 'Cancel' to not include.");
+//  d. numbers
+    var inputNumbers = confirm("Add Number characters? hit 'OK' to include lowercase characters. hit 'Cancel' to not include.");
+//  e. special characters
+    var inputSpecialChar = confirm("Add Special characters? hit 'OK' to include lowercase characters. hit 'Cancel' to not include.");
 
-//    b. lowercase
-//    c. uppercase
-//    d. numbers
-//    e. special characters
+
 // 2. Validate input
 // 3. Generate password based on criteria
 
