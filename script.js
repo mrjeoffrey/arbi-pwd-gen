19bf6cc471b04b808ca47bc552c0c6b9e266e512
mrjeoffrey <b.jeoffrey@gmail.com>
2022-01-03 03:42:25 -0800

    declared criteria variables.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// Assignment Code
+// Arbi Password Generator will allow a user to obtain a random password via prompts based on user selected password criteria.
+
 var generateBtn = document.querySelector("#generate");
+
+//Criteria Variables
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 // Define generatePassword
 function generatePassword() {
@@ -12,6 +19,9 @@ function generatePassword() {
 //    e. special characters
 // 2. Validate input
 // 3. Generate password based on criteria
+
+
+
 // 4. Display password
 
 
