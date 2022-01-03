d1d0500c5c6c2204b006955ab861e203da77009e
mrjeoffrey <b.jeoffrey@gmail.com>
2022-01-03 14:12:39 -0800

    Successfully created first prompt with validation. Used while loop for input outside of numeric range. Still need to restrict input to numerical values only.

--- script.js
+++ script.js
@@ -17,9 +17,18 @@ var inputSpecialChar;
 
 // Define generatePassword
 function generatePassword() {
-  console.log('Does this work?')
 // 1. Prompt user for password criteria
 //    a. Length 8 - 128 char
+  var inputLength = (prompt("Enter the character length for your password."));
+  // Error if not between 8 - 128 character range.
+  while (inputLength <= 7 || inputLength >= 129) {
+    alert("Try again. Password length must be between 8 - 128 characters.");
+    var inputLength = (prompt("Enter the character length for your password."));
+  }
+
+  // If user input in range, show length of password characters.
+  alert("Password length will be " + inputLength + " characters long.");
+
 //    b. lowercase
 //    c. uppercase
 //    d. numbers
