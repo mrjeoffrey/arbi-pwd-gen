d56880e9ea0128ef596f5e789a1305254d99b58f
mrjeoffrey <b.jeoffrey@gmail.com>
2022-01-03 13:54:28 -0800

    declared User Input Variables to begin prompts.

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
+// User Input for Password
+var inputLength = "";
+var inputLowerCase;
+var inputUpperCase;
+var inputNumbers;
+var inputSpecialChar;
+
 // Define generatePassword
 function generatePassword() {
   console.log('Does this work?')
