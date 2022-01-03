06d00a146c11cae815639ef160ed05e4c3730980
mrjeoffrey <b.jeoffrey@gmail.com>
2022-01-02 23:45:59 -0800

    assessed if script.js. identified broken/missing code. placed acceptance criteria inside script.js after creating missing function generatePassword.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define generatePassword
+function generatePassword() {
+  console.log('Does this work?')
+// 1. Prompt user for password criteria
+//    a. Length 8 - 128 char
+//    b. lowercase
+//    c. uppercase
+//    d. numbers
+//    e. special characters
+// 2. Validate input
+// 3. Generate password based on criteria
+// 4. Display password
+
+
+// remove undefined  
+  return "generated password here"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
